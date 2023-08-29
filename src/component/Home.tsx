baeaae5554607764747503224fb6e7f40096cec7
Anup <anuppoudel60@gmail.com>
2023-08-29 17:49:46 +1000

    navbar done

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { Container, Typography, Stack, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const languages = [
+  { name: "Japanese", code: "ja" },
+  { name: "Hindi", code: "hi" },
+  { name: "Spanish", code: "sh" },
+  { name: "French", code: "fr" },
+];
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  const languageHandler = (language: string): void => {
+    navigate(`/learn?language=${language}`);
+  };
+
+  return (
+    <Container maxWidth={"sm"}>
+      <Typography variant="h3" p={"2rem"} textAlign={"center"}>
+        Welcome, begin your journey of learning.
+      </Typography>
+      <Stack
+        direction={"row"}
+        spacing={"2rem"}
+        p={"2rem"}
+        alignItems={"center"}
+        justifyContent={"center"}
+      >
+        {languages.map((l) => (
+          <Button onClick={() => languageHandler(l.code)} key={l.code}>
+            {l.name}
+          </Button>
+        ))}
+      </Stack>
+      <Typography variant="h3" p={"2rem"} textAlign={"center"}>
+        Choose one language from above
+      </Typography>
+    </Container>
+  );
 };
 
 export default Home;
--- src/component/Learning.tsx
+++ src/component/Learning.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 const Learning = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const params = useSearchParams()[0].get("language") as LangType;
+
+  const navigate = useNavigate();
+
+  const nextHandler = (): void => {
+    setCount((prev) => prev + 1);
+  };
+
   return <div>Learning</div>;
 };
 
