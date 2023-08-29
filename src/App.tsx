38bc88f371ec4bb6557ee82f82dc5463f1a406e3
Anup <anuppoudel60@gmail.com>
2023-08-29 16:55:04 +1000

    routing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/Header";
+import Loader from "./component/Loader";
+
+const Home = lazy(() => import("./component/Home"));
+const Learning = lazy(() => import("./component/Learning"));
+const Quiz = lazy(() => import("./component/Quiz"));
+const Result = lazy(() => import("./component/Result"));
+const Login = lazy(() => import("./component/Login"));
 
 const App = () => {
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/learn" element={<Learning />}></Route>
+          <Route path="/quiz" element={<Quiz />}></Route>
+          <Route path="/result" element={<Result />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </Suspense>
+    </Router>
   );
 };
 
