a2b7bf0203bf43c87345800aaea36be4472e5e48
Anup <anuppoudel60@gmail.com>
2023-08-31 09:24:11 +1000

    redux added

---
+++ src/utils/feature.ts
@@ -0,0 +1,12 @@
+// import axios from "axios";
+// import { generate } from "random-words";
+
+// export const fetchWords = async () =>{
+//     try {
+//        const words =  generate(8)
+
+//     } catch (error) {
+
+//     }
+
+// }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,16 @@
 /// <reference types="vite/client" />
 
 type LangType = "ja" | "hi" | "es" | "fr";
+
+type WordType = {
+  word: string;
+  meaning: string;
+  options: string[];
+};
+
+type StateType = {
+  loading: boolean;
+  result: string[];
+  error?: string;
+  words: WordType[];
+};
